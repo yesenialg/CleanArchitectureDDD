873fbb160d5c12b9cf8e437e9fd663d81ecd64cd
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2024-05-02 14:43:12 -0500

    project

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+StreamerDbContext dbContext = new();
+
+void QueryStreaming()
+{
+    var streamers = dbContext.Streamers.ToList();
+    foreach(var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
+
+async Task AddNewRecords()
+{
+
+    Streamer streamer = new()
+    {
+        Nombre = "Amazon Prime",
+        Url = "https://www.amazonprime.com"
+    };
+
+    dbContext!.Streamers!.Add(streamer);
+
+    await dbContext.SaveChangesAsync();
+
+    var movies = new List<Video>
+{
+    new Video
+    {
+        Nombre = "Batman",
+        StreamerId = streamer.Id,
+    },
+    new Video
+    {
+        Nombre = "Jurassic",
+        StreamerId = streamer.Id,
+    },
+    new Video
+    {
+        Nombre = "Superman",
+        StreamerId = 1,
+    }
+};
+
+    await dbContext.AddRangeAsync(movies);
+
+    await dbContext.SaveChangesAsync();
+}
---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost,1433; 
+                    Initial Catalog=Streamer;User Id=SA;Password=admin123;TrustServerCertificate=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //FluentAPI en este caso esta parte es opcional, ya que usamos la convencion de EF
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Relación muchos a muchos Actores - Videos
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
